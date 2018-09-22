217ffc1425a2b7520c9ffcb9247365b9941c8236
tracer-ics2018 <tracer@njuics.org>
2018-09-22 07:39:17 +0000

    >  compile 171180541 gwynsmile Linux 8adcd289d027 4.9.93-linuxkit-aufs #1 SMP Wed Jun 6 16:55:56 UTC 2018 x86_64 GNU/Linux  07:39:17 up 21:01,  3 users,  load average: 0.01, 0.01, 0.00 7b83a3d8ce687a19d843b72e7efb2ac18b697330

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[]) {
   /* Receive commands from user. */
   //ui_mainloop(is_batch_mode);
   
-  FILE *fp = fopen("~/ics2018/nemu/tools/gen-expr/input", "r");
+  FILE *fp = fopen("~/ics2018", "r");
   if(fp == NULL)
     perror("input not found");
   char* ex = NULL;
