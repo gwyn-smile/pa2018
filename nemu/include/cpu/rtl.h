93da8fad81b5e7a3da6b530963e122b72d17dd9a
tracer-ics2018 <tracer@njuics.org>
2018-10-07 15:02:40 +0000

    >  compile 171180541 gwynsmile Linux 8adcd289d027 4.9.93-linuxkit-aufs #1 SMP Wed Jun 6 16:55:56 UTC 2018 x86_64 GNU/Linux  15:02:40 up 1 day,  8:17,  8 users,  load average: 0.14, 0.04, 0.01 220b976ee472a3d6f34fda09472904b572f0895e

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -167,7 +167,7 @@ static inline void rtl_top(rtlreg_t* dest) {
 	int i;
 	for(i=9; i>=0; i--) {
 		*dest = paddr_read(cpu.esp + i*4, 4);
-		Log("%d: %08x", i, *dest);
+		Log("0x%08x: %08x", cpu.esp+i*4, *dest);
 	}
 }
 
