a4f6f98b807cfed8775be085947cb9f021d23453
tracer-ics2018 <tracer@njuics.org>
2018-10-07 10:11:04 +0000

    >  compile 171180541 gwynsmile Linux 8adcd289d027 4.9.93-linuxkit-aufs #1 SMP Wed Jun 6 16:55:56 UTC 2018 x86_64 GNU/Linux  10:11:04 up 1 day,  4:21,  7 users,  load average: 0.08, 0.02, 0.01 a495dd7ab6f514291d22881b53907091cdf0f4a1

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -173,7 +173,7 @@ static inline void rtl_push(const rtlreg_t* src1) {
 static inline void rtl_pop(rtlreg_t* dest) {
   // dest <- M[esp]
   // esp <- esp + 4
-  paddr_write(cpu.esp, *dest, 32);
+  *dest = paddr_read(cpu.esp, 4);
 	cpu.esp = cpu.esp + 4;
 }
 
