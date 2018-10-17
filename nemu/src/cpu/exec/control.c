316b61fbb3ffc433443d683a62c835652aaf5480
tracer-ics2018 <tracer@njuics.org>
2018-10-17 15:53:54 +0000

    >  compile 171180541 gwynsmile Linux a4743ef8abdc 4.9.93-linuxkit-aufs #1 SMP Wed Jun 6 16:55:56 UTC 2018 x86_64 GNU/Linux  15:53:54 up 1 day, 19:32, 11 users,  load average: 0.00, 0.00, 0.00 9a4b33b949079ee9f7c03a583ddb97b27c3e1f22

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -27,7 +27,7 @@ make_EHelper(jmp_rm) {
 make_EHelper(call) {
   // the target address is calculated at the decode stage
   rtl_push(eip);
-	Log("the call intend to push %x", *eip);	
+	//Log("the call intend to push %x", *eip);	
 	rtl_j(decoding.jmp_eip);	
   print_asm("call %x", decoding.jmp_eip);
 }
