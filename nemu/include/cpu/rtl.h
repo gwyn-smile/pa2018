6d816f9ee8d7e5fa76f26b54d6228c3059543360
tracer-ics2018 <tracer@njuics.org>
2018-10-27 07:38:26 +0000

    >  compile 171180541 gwynsmile Linux a4743ef8abdc 4.9.93-linuxkit-aufs #1 SMP Wed Jun 6 16:55:56 UTC 2018 x86_64 GNU/Linux  07:38:26 up 3 days,  1:10, 11 users,  load average: 0.10, 0.04, 0.01 ba80ff695e226cdcc2e6e1bfd60ed628d6a78fcf

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -177,7 +177,7 @@ static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
 
 static inline void rtl_top(rtlreg_t* dest) {
 	int i;
-	for(i=9; i>=0; i--) {
+	for(i=2; i>=0; i--) {
 		*dest = paddr_read(cpu.esp + i*4, 4);
 		Log("0x%08x: %08x", cpu.esp+i*4, *dest);
 	}
--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -32,7 +32,7 @@ make_EHelper(call) {
 }
 
 make_EHelper(ret) {
-  //rtl_top(&id_dest->val);
+  rtl_top(&id_dest->val);
 	rtl_pop(eip);
 	rtl_j(*eip);
   print_asm("ret");
