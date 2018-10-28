ac6842e0b4d66aa19c8c631dff0806dd936ce37f
tracer-ics2018 <tracer@njuics.org>
2018-10-28 12:41:21 +0000

    >  run 171180541 gwynsmile Linux a4743ef8abdc 4.9.93-linuxkit-aufs #1 SMP Wed Jun 6 16:55:56 UTC 2018 x86_64 GNU/Linux  12:41:21 up 3 days, 19:13, 11 users,  load average: 0.00, 0.02, 0.00 9c6924cf55d868a7dbd84dd884d2fcd5fb6503eb

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -3,6 +3,16 @@
 
 #ifndef __ISA_NATIVE__
 
+void str_tra(char* oper, size_t len) {
+	char ret[256];
+	size_t i;
+	for(i = 0;i <= len - 1;i++) {
+		ret[i] = *(oper + len - i - 1);
+	}
+	ret[i] = '\0';
+	strncpy(oper, ret, len);
+}
+
 int printf(const char *fmt, ...) {
   assert(0);
 	return 0;
@@ -14,7 +24,51 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  assert(0);
+  va_list ap;
+	int32_t d;
+	char* s;
+	char c;
+	va_start(ap, fmt);
+  int i = 0;
+	char tran[256];
+	while(fmt[i] != '\0') {
+		if(fmt[i] != '%') {
+			tran[0] = fmt[i];
+			tran[1] = '\0';
+			strcat(out, tran);
+			i++;
+			continue;
+		}
+		
+		i++;
+		switch(fmt[i]) {
+			case 'c': {
+				c = (char)va_arg(ap, int);
+				tran[0] = c;
+				tran[1] = '\0';
+				strcat(out, tran);
+			} break;
+
+			case 's': {
+				s = va_arg(ap, char*);
+				strcat(out, s);
+			} break;
+
+			case 'd': {
+				int num, ct = 0;
+				d = (int32_t)va_arg(ap, int32_t);
+				while(d) {
+					num = d % 10;
+					d = d / 10;
+					tran[ct] = num - '0';
+					ct++;
+				}
+				tran[ct] = '\0';
+				str_tra(tran, strlen(tran));
+			} break;
+		}
+	}
+	va_end(ap);
 	return 0;
 }
 
