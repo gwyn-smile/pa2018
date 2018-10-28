4437aad5b7dc3bda60f208b18e3b359a3364f26a
tracer-ics2018 <tracer@njuics.org>
2018-10-28 03:06:51 +0000

    >  compile 171180541 gwynsmile Linux a4743ef8abdc 4.9.93-linuxkit-aufs #1 SMP Wed Jun 6 16:55:56 UTC 2018 x86_64 GNU/Linux  03:06:51 up 3 days, 12:31, 11 users,  load average: 0.10, 0.04, 0.01 85789764ec374bb1a1b4e04e3758ff68c73a05e5

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -172,7 +172,9 @@ static inline void rtl_not(rtlreg_t *dest, const rtlreg_t* src1) {
 
 static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
-  TODO();
+  int tmp = *src1;
+	tmp = (tmp << (32 - (width << 3))) >> (32 - (width << 3));
+	*dest = tmp;
 }
 
 static inline void rtl_top(rtlreg_t* dest) {
