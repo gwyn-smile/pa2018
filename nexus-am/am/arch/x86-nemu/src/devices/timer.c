9841ff6c84f4e6ea6d50f1bccccc25351b5c9300
tracer-ics2018 <tracer@njuics.org>
2018-10-31 15:24:16 +0000

    >  run 171180541 gwynsmile Linux a4743ef8abdc 4.9.93-linuxkit-aufs #1 SMP Wed Jun 6 16:55:56 UTC 2018 x86_64 GNU/Linux  15:24:16 up  3:53, 12 users,  load average: 0.00, 0.00, 0.00 21b2034e88d96405975491d01782c19c8ff6d012

--- nexus-am/am/arch/x86-nemu/src/devices/timer.c
+++ nexus-am/am/arch/x86-nemu/src/devices/timer.c
@@ -6,8 +6,8 @@ size_t timer_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_TIMER_UPTIME: {
       _UptimeReg *uptime = (_UptimeReg *)buf;
-      uptime->hi = 0;
-      uptime->lo = 0;
+      uptime->hi = ((_UptimeReg*)buf)->hi;
+      uptime->lo = ((_UptimeReg*)buf)->lo;
       return sizeof(_UptimeReg);
     }
     case _DEVREG_TIMER_DATE: {
